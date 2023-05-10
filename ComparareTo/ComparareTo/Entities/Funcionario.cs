ca6c826b87c5b903f4995a2fd77a21dbd0b5ddbe
Victor Oliveira <victor.oliveira20@icloud.com>
2023-05-10 19:10:17 -0300

    Realizado exercicio CompareTo

---
+++ ComparareTo/ComparareTo/Entities/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+namespace ComparareTo.Entities
+{
+    internal class Funcionario : IComparable
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(string csvFuncionario) 
+        {
+            string[] vect = csvFuncionario.Split(',');
+            Nome = vect[0];
+            Salario = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if(!(obj is Funcionario))
+            {
+                throw new ArgumentException("Comparing error: Argument is not an employee ");
+
+            }
+
+            Funcionario outro = obj as Funcionario;
+            return Nome.CompareTo(outro.Nome);
+        }
+    }
+}
---
+++ ComparareTo/ComparareTo/Program.cs
@@ -0,0 +1,34 @@
+﻿using ComparareTo.Entities;
+
+namespace ComparareTo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\data\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Funcionario> list = new List<Funcionario>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Funcionario(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach(Funcionario emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
