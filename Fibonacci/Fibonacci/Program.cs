36ce3ca44f15b60e08393b1bdd3e31c7d1fb2aeb
Victor Oliveira <victor.oliveira20@icloud.com>
2023-05-13 09:56:48 -0300

    Exercicio CompareTo e um teste de logica de programação

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeiro = 0;
+            int segundo = 1;
+
+            Console.Write(primeiro + " " + segundo);
+            for (int i = 3; i <= 15; i++)
+            {
+
+                int terceiro = primeiro + segundo;
+                primeiro = segundo;
+                segundo = terceiro;
+
+                Console.Write(" " + segundo);
+            }
+
+            
+
+
+        }
+    }
+}
+
---
+++ TesteDeLogica/TesteDeLogica/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace TesteDeLogica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int antepenultimo = 5;
+            int penultimo = 6;            
+            int ultimo = 8;
+
+            for (int i = 4; i <= 50; i++)
+            {
+                int _50Termo = penultimo + antepenultimo;
+                antepenultimo = penultimo;
+                penultimo = ultimo;
+                ultimo = _50Termo;
+            }
+
+            Console.WriteLine(ultimo);
+
+        }
+    }
+}
