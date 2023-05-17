219c05aaec24951b5e9c078b94811adec2c3c5aa
Victor Oliveira <victor.oliveira20@icloud.com>
2023-05-17 18:37:00 -0300

    Hash e SortedSet

---
+++ HashESortedSet/HashESortedSet/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace HashESortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Computer"));
+
+            foreach (string item in set)
+            {
+                Console.WriteLine(item);
+            }
+
+            //SortedSet
+            Console.WriteLine();
+            Console.WriteLine("*********SORTEDSET*********");
+            Console.WriteLine();
+
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+
+            //union
+
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+
+            PrintCollection(c);
+
+            //intersection
+
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+
+            PrintCollection(d);
+
+            //difference
+
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            PrintCollection(e);
+
+
+           
+        }
+
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+        foreach (T item in collection)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
