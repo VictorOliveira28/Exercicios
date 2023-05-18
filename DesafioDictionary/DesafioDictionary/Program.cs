993c17d5579b4cd61b76a609ad255a5236ffcc64
Victor Oliveira <victor.oliveira20@icloud.com>
2023-05-18 18:38:16 -0300

    Desafio de dictionary resolvido

---
+++ DesafioDictionary/DesafioDictionary/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace DesafioDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"c:\data\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] contagemVotos = sr.ReadLine().Split(',');
+                        string name = contagemVotos[0];
+                        int votos = int.Parse(contagemVotos[1]);
+
+                        if (keyValuePairs.ContainsKey(name))
+                        {
+                            keyValuePairs[name] += votos;
+                        }
+                        else
+                        {
+                            keyValuePairs[name] = votos;
+                        }
+                    }
+
+                 foreach(var kvp in keyValuePairs)
+                    {
+                        Console.WriteLine(kvp.Key + ": " + kvp.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+}
--- DictionaryExemplo/DictionaryExemplo/Program.cs
+++ DictionaryExemplo/DictionaryExemplo/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DictionaryExemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "Maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["Phone"] = "99712234";
+            cookies["Phone"] = "83737388";
+
+            Console.WriteLine(cookies["email"]);
+            Console.WriteLine(cookies["Phone"]);
+
+            cookies.Remove("email");
+
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("There's no 'email' key.");
+            }
+
+            Console.WriteLine("Size: " + cookies.Count);
+            Console.WriteLine("All cookies");
+            foreach(KeyValuePair<string, string> kvp in cookies)
+            {
+                Console.WriteLine(kvp.Key + ": " + kvp.Value);
+            }
+
+        }
+    }
+}
