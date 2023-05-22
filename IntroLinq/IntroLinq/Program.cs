8528aea2f246fec914c2afb9a432839c4df1daa0
Victor Oliveira <victor.oliveira20@icloud.com>
2023-05-22 19:37:03 -0300

    Introducao a Linq

---
+++ IntroLinq/IntroLinq/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+namespace IntroLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Specify data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            //Define the query expression
+
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+
+            //Execute the Query
+            foreach(int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
