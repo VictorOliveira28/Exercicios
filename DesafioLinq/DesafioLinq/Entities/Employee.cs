81840a480c4777b70ea33c168b3033a9be594079
Victor Oliveira <victor.oliveira20@icloud.com>
2023-05-23 16:54:08 -0300

    Desafio realizado, utilizando streamreader e Linq

---
+++ DesafioLinq/DesafioLinq/Entities/Employee.cs
@@ -0,0 +1,21 @@
+﻿
+namespace DesafioLinq.Entities
+{
+    internal class Employee
+    {
+        public Employee()
+        {
+        }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+    }
+}
---
+++ DesafioLinq/DesafioLinq/Program.cs
@@ -0,0 +1,49 @@
+﻿using DesafioLinq.Entities;
+using System.Globalization;
+
+
+namespace DesafioLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+            Console.Write("Enter salary: ");
+            double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            List<Employee> employees = new List<Employee>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fields = sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        string email = fields[1];
+                        double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+
+                        employees.Add(new Employee(name, email, salary));
+                    }
+
+                    var emails = employees.Where(e => e.Salary > limit).OrderBy(e => e.Email).Select(e => e.Email);
+                    var sum = employees.Where(s => s.Name[0] == 'M').Sum(s => s.Salary);
+                    Console.WriteLine("Email of people whose salary is more than " + limit.ToString("F2", CultureInfo.InvariantCulture));
+                    foreach (string email in emails)
+                    {
+                        Console.WriteLine(email);
+                    }
+                    Console.WriteLine("Sum of salary of people whose name starts with 'M': " + sum.ToString("F2", CultureInfo.InvariantCulture));
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
